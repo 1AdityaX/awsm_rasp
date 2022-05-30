0ed1eeae19c79d50d4bd726038284fb1b4a90581
ChaosXsniffer <biscuitbot07@gmail.com>
2022-05-30 11:22:45 +0530

    package update

--- setup.sh
+++ setup.sh
@@ -11,6 +11,11 @@ sudo apt-get install -y libhdf5-dev
 sudo apt-get install -y libhdf5-serial-dev
 sudo apt-get install -y libatlas-base-dev
 sudo apt-get install -y libjasper-dev 
+sudo apt-get install -y libopenjp2-7
+sudo apt-get install -y libavcodec-dev
+sudo apt-get install -y libavformat-dev
+sudo apt-get install -y libswscale-dev
+sudo apt-get install -y libgtk-3-dev
 
 echo "Installing Python Libraries"
 
