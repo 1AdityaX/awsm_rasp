c448915c1a5a3fd8a54ea0b7fb3e52839677cbd9
ChaosXsniffer <biscuitbot07@gmail.com>
2022-05-30 11:34:25 +0530

    .

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-from RPLCD.gpio import CharLCD
+# from RPLCD.gpio import CharLCD
 from utility.utility import classify_image
 from utility.servo import Servo
 from utility.rfid import Rfid
-from RPLCD import CharLCD
+# from RPLCD import CharLCD
 import RPi.GPIO as GPIO
 import time
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-lcd = CharLCD(pin_rs=6, pin_rw=5, pin_e=25, pins_data=[16, 26, 13, 12],
-              numbering_mode=GPIO.BCM, cols=20, rows=4, auto_linebreaks=True)
+# lcd = CharLCD(pin_rs=6, pin_rw=5, pin_e=25, pins_data=[16, 26, 13, 12],
+#               numbering_mode=GPIO.BCM, cols=20, rows=4, auto_linebreaks=True)
 
 def main():
     time.sleep(1)
-    lcd.clear()
+    # lcd.clear()
     time.sleep(1)
-    lcd.write_string(u'Place your rfid card')
+    # lcd.write_string(u'Place your rfid card')
     time.sleep(2)
     try:
         id,text = reader.read()
         data = dict(eval(text))
     finally:
         GPIO.cleanup([9, 10, 11, 8])
-    lcd.clear()
+    # lcd.clear()
     
-    lcd.write_string(f'RFID: {id}')
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string(f'Name: {data["name"]}')
-    lcd.cursor_pos = (2, 0)
-    lcd.write_string(f'Points: {data["points"]}')
-    lcd.cursor_pos = (3, 0)
-    lcd.write_string(f'Class: {data["class"]}')
+    # lcd.write_string(f'RFID: {id}')
+    # lcd.cursor_pos = (1, 0)
+    # lcd.write_string(f'Name: {data["name"]}')
+    # lcd.cursor_pos = (2, 0)
+    # lcd.write_string(f'Points: {data["points"]}')
+    # lcd.cursor_pos = (3, 0)
+    # lcd.write_string(f'Class: {data["class"]}')
     
     classfication = classify_image()
 
@@ -39,24 +39,24 @@ def main():
     servo2 = Servo(27)
     top_servo = Servo(22)
 
-    lcd.clear()
+    # lcd.clear()
     if classfication == "plastic":
-        lcd.write_string("Plastic")
+        # lcd.write_string("Plastic")
         servo1.left()
         servo2.left()
 
     elif classfication == "metal":
-        lcd.write_string("Metal")
+        # lcd.write_string("Metal")
         servo1.left()
         servo2.right()
 
     elif classfication == "paper" or classfication == "cardboard":
-        lcd.write_string("Paper")
+        # lcd.write_string("Paper")
         servo1.right()
         servo2.left()
 
     else:
-        lcd.write_string("Other")
+        # lcd.write_string("Other")
         servo1.right()
         servo2.right()
 
@@ -77,6 +77,6 @@ try:
         except Exception as e:
             raise e            
 except KeyboardInterrupt:
-    lcd.clear()
+    # lcd.clear()
     GPIO.cleanup()
 
