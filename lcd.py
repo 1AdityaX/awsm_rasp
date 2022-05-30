20ff88c5f5d00db77a3b77fcc07fc89af288285b
ChaosXsniffer <biscuitbot07@gmail.com>
2022-05-30 11:22:34 +0530

    pins update

--- lcd.py
+++ lcd.py
@@ -1,7 +1,7 @@
 from RPLCD.gpio import CharLCD
 import RPi.GPIO as GPIO
-lcd = CharLCD(pin_rs=31, pin_rw=29, pin_e=22, pins_data=[36, 37, 33, 32],
-              numbering_mode=GPIO.BOARD, cols=20, rows=4, auto_linebreaks=True)
+lcd = CharLCD(pin_rs=6, pin_rw=5, pin_e=25, pins_data=[16, 26, 13, 12],
+              numbering_mode=GPIO.BCM, cols=20, rows=4, auto_linebreaks=True)
 #lcd.clear()
 lcd.write_string('Place your rfid card')
 lcd.clear()
