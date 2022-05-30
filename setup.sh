37149d384048e20af7663c5f1c49f7a500575aa1
ChaosXsniffer <biscuitbot07@gmail.com>
2022-05-30 09:59:46 +0530

    pip install cmd

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install git
-sudo apt-get -y install python-pip
-sudo apt-get -y install python
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-is-python3
 sudo apt-get -y dist-upgrade
 
 echo "Installing Required Packages"
 
-sudo apt-get install libhdf5-dev
-sudo apt-get install libhdf5-serial-dev
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libjasper-dev 
+sudo apt-get install -y libhdf5-dev
+sudo apt-get install -y libhdf5-serial-dev
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libjasper-dev 
 
 echo "Installing Python Libraries"
 
-sudo python pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Done!"
 
