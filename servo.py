4bc3779ae4a6b80a0a4f977e909c87fadd2788bc
ChaosXsniffer <biscuitbot07@gmail.com>
2022-05-31 12:43:58 +0530

    pins update

--- servo.py
+++ servo.py
@@ -1,10 +1,14 @@
 from utility.servo import Servo
 
-servo1 = Servo(17)
-servo2 = Servo(27)
-top_servo = Servo(22)
+servo1 = Servo(5)
+servo2 = Servo(6)
+top_servo = Servo(13)
 
+servo1.right()
+servo1.left()
 servo1.middle()
+servo2.right()
+servo2.left()
 servo2.middle()
 top_servo.open()
 top_servo.close()
