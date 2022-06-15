3af9394e4bcde4f4f6d96257c71e0aa08966066a
ChaosXsniffer <biscuitbot07@gmail.com>
2022-06-15 09:59:32 +0530

    lcd test update

--- lcd.py
+++ lcd.py
@@ -1,9 +1,9 @@
-from RPLCD.gpio import CharLCD
+from RPLCD.i2c import CharLCD
 import RPi.GPIO as GPIO
-lcd = CharLCD(pin_rs=6, pin_rw=5, pin_e=25, pins_data=[16, 26, 13, 12],
-              numbering_mode=GPIO.BCM, cols=20, rows=4, auto_linebreaks=True)
-#lcd.clear()
+import time
+lcd = CharLCD('PCF8574', 0x27)
 lcd.write_string('Place your rfid card')
+time.sleep(3)
 lcd.clear()
 
 GPIO.cleanup()
