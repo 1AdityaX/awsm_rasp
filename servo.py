7e7dd3aaeebae8e283b5dbaebb9876b0c4ba83a6
ChaosXsniffer <biscuitbot07@gmail.com>
2022-06-15 09:33:33 +0530

    update

--- servo.py
+++ servo.py
@@ -1,9 +1,13 @@
 from utility.servo import Servo
 
-servo1 = Servo(17)
+servo1 = Servo(5)
 servo2 = Servo(6)
 top_servo = Servo(13)
 
 servo1.right()
 servo1.left()
 servo1.middle()
+
+servo2.right()
+servo2.left()
+servo2.middle()
