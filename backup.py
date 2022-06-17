33bc9d5003939477cc15797b4f01a20029092dd5
ChaosXsniffer <biscuitbot07@gmail.com>
2022-06-17 09:17:45 +0530

    backup file

---
+++ backup.py
@@ -0,0 +1,66 @@
+from utility.utility import classify_image
+from utility.servo import Servo
+from RPLCD.i2c import CharLCD
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+reader = SimpleMFRC522()
+
+lcd = CharLCD('PCF8574', 0x27)
+
+def main():
+    time.sleep(1)
+    lcd.clear()
+    time.sleep(1)
+    lcd.write_string(u'Running backup...')
+    time.sleep(2)
+    lcd.write_string(u'Place your trash in the deposit box')
+    time.sleep(4)
+    classfication = classify_image()
+
+    servo1 = Servo(5)
+    servo2 = Servo(6)
+    top_servo = Servo(13)
+
+    lcd.clear()
+    if classfication == "plastic":
+        lcd.write_string("Plastic")
+        time.sleep(1)
+        lcd.clear()
+
+    elif classfication == "metal":
+        lcd.write_string("Metal")
+        time.sleep(1)
+        lcd.clear()
+
+    elif classfication == "paper" or classfication == "cardboard":
+        lcd.write_string("Paper")
+        time.sleep(1)
+        lcd.clear()
+
+    else:
+        lcd.write_string("Other")
+        time.sleep(1)
+        lcd.clear()
+
+    top_servo.open()
+    top_servo.close()
+    servo1.middle()
+    servo2.middle()
+
+
+a = 1
+
+try:
+    while a == 1:
+        try:
+            main()
+        except EOFError:
+            pass
+        except Exception as e:
+            raise e            
+except KeyboardInterrupt:
+    lcd.clear()
+    GPIO.cleanup()
+
